9195f4f28b6bb5e62855322e044e93e31e054bbb
stani <svwielga4@gmail.com>
2025-09-03 09:28:06 -0500

    updating

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
   # POST /items or /items.json
   def create
     @item = Item.new(item_params)
-
+    @item.user = current_user
     respond_to do |format|
       if @item.save
         format.html { redirect_to @item, notice: "Item was successfully created." }
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  scope "/admin" do
+    resources :users
+  end 
+
   resources :items
   resources :users
   resources :roles
